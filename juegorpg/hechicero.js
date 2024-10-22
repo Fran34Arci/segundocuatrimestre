285a1496561213d0efc1f4b7716a299cf4424b68
Francisco <https://github.com/Fran34Arci/programacionWeb.git>
2024-10-21 21:56:34 -0300

    trabajo practico juego rpg

---
+++ juegorpg/hechicero.js
@@ -0,0 +1,100 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Hechicero = void 0;
+var personaje_1 = require("./personaje");
+var Hechicero = /** @class */ (function (_super) {
+    __extends(Hechicero, _super);
+    //constructor
+    function Hechicero(nombre, puntosDeVida, puntosGolpe, cantidadVidas, mana, manaFuego, manaTrueno) {
+        var _this = _super.call(this, nombre, puntosDeVida, puntosGolpe, cantidadVidas) || this;
+        _this.mana = mana;
+        _this.manaFuego = manaFuego;
+        _this.manaTrueno = manaTrueno;
+        return _this;
+    }
+    Hechicero.prototype.atacar = function () {
+        if (this.nivel == 1) {
+            console.log("Ataque normal de hechicero");
+            this.puntosGolpe + 10;
+            this.mana + 10;
+            this.chequearNivel();
+        }
+        else if (this.nivel == 2) {
+            console.log("Ataque ".concat(this.nivel, " de hechicero"));
+            this.puntosGolpe + 15;
+            this.mana + 15;
+            this.manaFuego + 15;
+            this.chequearNivel();
+        }
+        else if (this.nivel == 3) {
+            console.log("Ataque ".concat(this.nivel, " de hechicero"));
+            this.puntosGolpe + 20;
+            this.mana + 20;
+            this.manaFuego + 20;
+            this.manaTrueno + 25;
+            this.chequearNivel();
+        }
+    };
+    //ataques de Hechicero
+    Hechicero.prototype.ataqueFuego = function () {
+        if (this.manaFuego >= 50 && this.mana >= 15) {
+            this.mana - 15;
+            this.manaFuego - 50;
+            this.puntosGolpe + 10;
+            console.log("Ataque bola de fuego");
+        }
+        else {
+            console.log("tu nivel de mana y/o mana de Fuego es bajo no puedes ejecutar el ataque");
+        }
+    };
+    Hechicero.prototype.ataqueTrueno = function () {
+        if (this.manaFuego >= 75 && this.mana >= 20) {
+            this.mana - 20;
+            this.manaTrueno - 75;
+            this.puntosGolpe + 15;
+            console.log("Ataque de trueno");
+        }
+        else {
+            console.log("tu nivel de mana y/o mana de Trueno es bajo no puedes ejecutar el ataque");
+        }
+    };
+    //metodos: getters y setters 
+    Hechicero.prototype.getMana = function () {
+        return this.mana;
+    };
+    Hechicero.prototype.setMana = function (nuevoMana) {
+        this.mana = nuevoMana;
+    };
+    Hechicero.prototype.getManaFuego = function () {
+        return this.manaFuego;
+    };
+    Hechicero.prototype.setManaFuego = function (nuevomanaFuego) {
+        this.manaFuego = nuevomanaFuego;
+    };
+    Hechicero.prototype.getManaTrueno = function () {
+        return this.manaTrueno;
+    };
+    Hechicero.prototype.setManaTrueno = function (nuevomanaTrueno) {
+        this.manaTrueno = nuevomanaTrueno;
+    };
+    Hechicero.prototype.getDatosHechicero = function () {
+        return ("Nivel de Mana: ".concat(this.getMana(), "\n        Nivel de Mana Fuego: ").concat(this.getManaFuego(), "\n        Nivel de Mana Trueno: ").concat(this.getManaTrueno(), "\n        "));
+    };
+    return Hechicero;
+}(personaje_1.Personaje));
+exports.Hechicero = Hechicero;
---
+++ juegorpg/main.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hechicero_1 = require("./hechicero");
+var guerrero_1 = require("./guerrero");
+var arquero_1 = require("./arquero");
+//nombre, nivel, puntosDeVida, puntosGolpe,cantidadVidas, fuerza,espadaGrande, hacha) {
+var guerrero1 = new guerrero_1.Guerrero("Conan", 150, 0, 3, 100, false, false);
+//nombre, nivel, puntosDeVida, puntosGolpe,cantidadVidas, precision, flechasPlata, flechasOro)
+var arquero1 = new arquero_1.Arquero("Legolas", 120, 0, 3, 100, false, false);
+//nombre, nivel, puntosDeVida, puntosGolpe,cantidadVidas, mana, manaFuego,manaTrueno
+var hechicero1 = new hechicero_1.Hechicero("Merlin", 100, 0, 3, 100, 0, 0);
+console.log(guerrero1.getDatos() + guerrero1.getDatosGuerrero());
+console.log(hechicero1.getDatos() + hechicero1.getDatosHechicero());
+console.log(arquero1.getDatos() + arquero1.getDatosArquero());
+//Arquero
+for (var x = 0; x <= 10; x++) {
+    arquero1.esquivar();
+    arquero1.defender();
+    arquero1.atacar();
+    arquero1.ataqueFlechasOro();
+    arquero1.ataqueFlechasPlata();
+    console.log(arquero1.getDatos() + arquero1.getDatosArquero());
+}
+//Guerrero
+for (var x = 0; x <= 10; x++) {
+    guerrero1.esquivar();
+    guerrero1.defender();
+    guerrero1.atacar();
+    guerrero1.ataqueEspadaGrande();
+    guerrero1.ataqueHacha();
+    console.log(arquero1.getDatos() + arquero1.getDatosArquero());
+}
+//Hechicero
+for (var x = 0; x <= 10; x++) {
+    hechicero1.esquivar();
+    hechicero1.defender();
+    hechicero1.atacar();
+    hechicero1.ataqueFuego();
+    hechicero1.ataqueTrueno();
+    hechicero1.atacar();
+    console.log(arquero1.getDatos() + arquero1.getDatosArquero());
+}
